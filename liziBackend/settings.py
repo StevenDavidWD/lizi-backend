846cfbce45592a0a1abb194a1b18455bdc5cb7ab
sundaydx <sundaydx.dreaming@gmail.com>
2015-04-01 10:55:05 +0800

    change the file

--- liziBackend/settings.py
+++ liziBackend/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'liziBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lizi',
+        'USER': 'root',
+        'PASSWORD': 'ding1994',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
